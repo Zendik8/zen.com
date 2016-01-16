7905c00ee6d270a3853c21d2d684a8867dc3f378
Zendik8 <nazary88>
2016-01-16 19:41:09 +0000

    first setting app

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -1,17 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Blog extends CI_Controller {
 
     public function index()
     {
         $data = array(
-            'title' => 'main_title'
+            'title' => 'blog_title'
         );
 
         $this->load->view('head_view', $data);
         $this->load->view('header_view');
-        $this->load->view('main');
+        $this->load->view('article_view');
         $this->load->view('footer_view');
+
     }
 }
--- application/views/main.php
+++ application/views/main.php
@@ -1,16 +1,3 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-
-</head>
-<body>
-
-<div id="container">
+<div class="container">
 	<h1>Welcome Zendik!</h1>
-</body>
-</html>
+</div>
